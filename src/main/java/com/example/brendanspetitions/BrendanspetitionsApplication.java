b7d8ea2b779e5a535079b74f7df769b1b7b934f3
brendan-galway <qugers@yahoo.com>
2024-11-23 14:54:08 +0000

    Initial commit

---
+++ src/main/java/com/example/brendanspetitions/BrendanspetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.brendanspetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrendanspetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrendanspetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/brendanspetitions/BrendanspetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.brendanspetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrendanspetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
